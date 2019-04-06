d1dbec4d7effaa324ed2c0cc18a0333b72eb1065
Matt Werner <mrwerner392@gmail.com>
2019-04-06 17:20:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+// deletes 'key' from a clone of the given object and returns the new object
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;/
+}
